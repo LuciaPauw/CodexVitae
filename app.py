aaf044c6958be19d805dbc5d6140a06781ff48b8
Lucia <lucia@Lucias-MacBook-Pro.local>
2025-08-26 10:00:02 +0200

    Dior new look (frontend)

--- app.py
+++ app.py
@@ -6,18 +6,42 @@ st.set_page_config(
     page_title="Codex Vitae"
 )
 
-st.image("./static/1.png", use_container_width=True)
+# sidebar
+st.sidebar.image("./static/1.png", use_container_width=True)
 
-# col1, col2, col3 = st.columns([1, 3, 1])
-# with col2:
-#     st.image("./static/2.png", width=800)
+st.sidebar.markdown(
+    """
+    <div style="background-color:#dbe4f0; padding: 1rem; 
+                border-radius: 8px; margin-bottom: 1rem;
+                color: #304674;">
+        <b>Personal brand promise</b><br>
+        I am committed to excellence in data science, AI, and entrepreneurship.<br>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
-col1, col2, col3 = st.columns([1, 3, 1])
+st.sidebar.image("./static/Pauw.png", use_container_width=True)
+
+# main panel
+col1, col2 = st.columns([1, 4])
+with col1:
+    st.image("./static/chatbot2.png", width=200)
 with col2:
-    st.text("Thinking of hiring me? Let this agent answer your questions about who I am and what I can do.")
+    st.subheader("Thinking of hiring me?")
+    st.text("Let this agent answer your questions about who I am and what I can do.")
 
-
-user_question = st.text_input("Ask away!")
+st.markdown(
+    """
+    <style>
+        .stTextInput input {
+            background-color: white !important;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+user_question = st.text_input("Ask away!", placeholder="E.g. What are Lucia's strongest technical skills?")
 
 if st.button("Submit") and user_question:
     with st.spinner("Generating answer..."):
